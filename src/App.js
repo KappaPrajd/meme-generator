a2502188dadb086c228cbc847bf07a6e566b4557
KappaPrajd <tomaszmolski22@wp.pl>
2021-01-26 02:20:00 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [header, setHeader] = useState("Choose template");
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState();
+
+  useEffect(() => {
+    const fetchTemplates = async () => {
+      const { data } = await axios.get("https://api.imgflip.com/get_memes");
+      const memes = data.data.memes.filter((meme, index) => index < 9);
+
+      setTemplates(memes);
+    };
+
+    fetchTemplates();
+  }, []);
+
+  useEffect(() => {
+    if (template) {
+      setHeader("Insert text");
+    }
+  }, [template]);
+
+  const renderContent = () => {
+    //waiting for templates to load
+    if (templates.length === 0) {
+      return <p>Loading...</p>;
+    }
+
+    //show templates if none selected
+    else if (templates.length > 0 && !template) {
+      const content = templates.map((el, index) => {
+        return (
+          <img
+            className="template"
+            src={el.url}
+            alt={el.name}
+            key={index}
+            onClick={() => setTemplate(el)}
+          ></img>
+        );
+      });
+
+      return <div className="templates">{content}</div>;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>{header}</h1>
+      {renderContent()}
+    </React.Fragment>
   );
-}
+};
 
 export default App;
